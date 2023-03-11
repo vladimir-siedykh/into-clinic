d96e06262e485c73bdf702f9054db7026b404796
volodymyr-siedykh <datrimius@gmail.com>
2023-03-11 19:26:10 +0200

    v4.6 sitemap

--- next.config.js
+++ next.config.js
@@ -2,6 +2,14 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-}
+  async rewrites() {
+    return [
+      {
+        source: '/sitemap.xml',
+        destination: '/api/sitemap',
+      },
+    ];
+  },
+};
 
 module.exports = nextConfig
